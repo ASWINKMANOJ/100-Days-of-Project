b763df06096ce4204aa6e4891f7afe39f00421f0
ASWIN K MANOJ <aswinkmanoj101@gmail.com>
2023-01-05 15:07:22 +0530

    tkinter

--- Intermediate/Day 27/tkinter_intro.py
+++ Intermediate/Day 27/tkinter_intro.py
@@ -5,6 +5,21 @@ window = tkinter.Tk()
 window.title("WINDOW")
 window.minsize(width=500, height=300)
 label = tkinter.Label(text="I'm a label", font=("Aerial", 23, "normal"))
-label.pack(side="right")
+label.pack(side="top")
+
+
+def buttonClicked():
+    global label
+    label.config(text=input.get())
+
+
+label["text"] = "New Text"
+label.config(text="New Text")
+button = tkinter.Button(text="Click ME", command=buttonClicked)
+button.pack()
+
+input = tkinter.Entry()
+input.pack()
+
 
 window.mainloop()
